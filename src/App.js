dcf05a534a5197db899085ce0d8978b8bf98418d
lutfor779 <mdlrrahman779@gmail.com>
2021-09-26 12:13:21 +0600

    header folder name correct

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Display from './components/Display/Display';
-import Header from './components/Header./Header';
+import Header from './components/Header/Header';
 
 function App() {
   return (
